f1fa301ea517943fb20e293cdd328dd9b99144b1
Dimas <dimashpt@gmail.com>
2023-09-18 22:35:01 +0700

    feat: install nextui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@nextui-org/react'
+
 export default function App() {
   return (
     <main className="flex w-full h-screen justify-center items-center">
-      <h1>Main Page</h1>
+      <Button color="primary">
+        Button
+      </Button>
     </main>
   )
 }
